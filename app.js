d41ad4f9abd159bb0156f66e9ef2385e7be9d7ca
Nimish-2612 <nimishsharma2612@gmail.com>
2025-07-01 17:40:06 +0530

    Modified backend

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ dotenv.config();
 const app = express();
 app.use(
   cors({
-    // origin: "http://localhost:5173"||"http://localhost:5174",
+    origin: "https://cyclebuddy-backend.onrender.com",
     credentials: true,
   })
 );
